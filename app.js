30f00e0d291eb35c983c936424cdd1b2da557f28
Briseid Pagador <bpagador@gmail.com>
2020-03-27 13:24:24 -0700

    added assets folder

---
+++ app.js
@@ -0,0 +1,23 @@
+// get a bunch of DOM elements 
+
+// go get html elements by their ids
+
+// we'll need all the inputs
+
+// we'll need spans to put the words into the mad lib
+
+// we'll need the container that holds the madlib
+
+// we'll need the container that holds all the inputs 
+
+// we'll need the button so we can add the event listener 
+
+const myButton = document.getElementById('button');
+
+// add event listeners 
+    // get the value of the inputs
+    // use the input values to SET the value of our spans
+    // hide the input container
+    // reveal the mad lib container 
+
+// add event listener to the button on the click event
