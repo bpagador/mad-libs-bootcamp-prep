295af9456285f5663e556569c7763eeb642da873
Briseid Pagador <bpagador@gmail.com>
2020-03-27 15:57:28 -0700

    added first input

--- app.js
+++ app.js
@@ -3,21 +3,31 @@
 // go get html elements by their ids
 
 // we'll need all the inputs
-
+const aFoodInput = document.getElementById('a-food-input');
 // we'll need spans to put the words into the mad lib
-
+const aFoodSpan = document.getElementById('a-food-span');
 // we'll need the container that holds the madlib
-
+const madlibContainer = document.getElementById('madlib-container');
 // we'll need the container that holds all the inputs 
-
+const inputContainer = document.getElementById('input-container');
 // we'll need the button so we can add the event listener 
-
 const myButton = document.getElementById('button');
 
-// add event listeners 
-    // get the value of the inputs
-    // use the input values to SET the value of our spans
-    // hide the input container
-    // reveal the mad lib container 
+// add event listeners
+function putAnswersIn() { 
+    
+// get the value of the inputs
+    const aFoodValue = aFoodInput.value;
+// use the input values to SET the value of our spans
+    aFoodSpan.textContent = aFoodValue;
+// hide the input container
+    inputContainer.style.visibility = 'hidden';
+// reveal the mad lib container 
+    madlibContainer.style.visibility = 'visible';
+    
+    console.log(aFoodValue);
+}
 
 // add event listener to the button on the click event
+myButton.addEventListener('click', putAnswersIn);
+
